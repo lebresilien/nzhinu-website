0251f368c4a4195cc593cb1b16ce7b3e1f978576
negro <petitotapondjou@gmail.com>
2024-01-19 15:45:13 +0100

    resolve icon issue

--- app/components/ui/icons.tsx
+++ app/components/ui/icons.tsx
@@ -1,6 +1,6 @@
 import { type SVGProps } from 'react'
 import { type IconName } from '@/app/types/name'
-import { cn } from '@/ /lib/utils'
+import { cn } from '@/lib/utils/cn'
 
 export { IconName }
 
