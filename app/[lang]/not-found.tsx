4d2849be0d2d469251f88ac707a6ac998937e5fa
negro <petitotapondjou@gmail.com>
2024-01-20 13:28:21 +0100

    added internalization to not found

--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -1,20 +1,32 @@
 import { Metadata } from "next"
 import Link from "next/link"
+import { headers } from 'next/headers'
+import { match } from '@formatjs/intl-localematcher'
+import Negotiator from 'negotiator'
+import { getDictionary } from "@/get-dictionary"
 
 export const metadata: Metadata = {
-  title: "404",
-  description: "Something went wrong",
+  title: "404"
 }
 
-export default function NotFound() {
+export default async function NotFound() {
+
+  let headers = { 'accept-language': 'fr,en;q=0.5' }
+  let languages = new Negotiator({ headers }).languages()
+  let locales = ['en', 'fr']
+  let defaultLocale = 'fr'
+  let lang = match(languages, locales, defaultLocale) 
+  // @ts-ignore
+  const dict = await getDictionary(lang)
+
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-gray-900">Page not found</h1>
+      <h1 className="text-2xl text-gray-900">{dict['not_found']['name']}</h1>
       <p className="text-sm text-gray-700">
-        The page you tried to access does not exist.
+        {dict['not_found']['exists']}
       </p>
-      <Link href="/" className="mt-4 underline text-base-regular text-gray-900">
-        Go to frontpage
+      <Link href="/" className="mt-4 underline text-gray-900">
+        {dict['not_found']['back']}
       </Link>
     </div>
   )
